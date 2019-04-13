e5eae418864004b4265977f273776a884c1edf90
Elma <etrako1@etf.unsa.ba>
2019-04-13 23:57:08 +0200

    auzuriranje

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -10,7 +10,7 @@ public class Controller {
     public Label fldEmail;
     public Label fldKorisnickoIme;
     public Label fldLozinka;
-    public ListView listaKorisnika;
+    public ListView<Korisnik> listaKorisnika;
 
     private KorisnikModel model=new KorisnikModel();
 
@@ -20,16 +20,38 @@ public class Controller {
 
     public void initialize() {
 
-        /*fldIme.textProperty().bindBidirectional(model.getTrenutniKorisnik().imeProperty());
-        fldPrezime.textProperty().bindBidirectional(model.getTrenutniKorisnik().prezimeProperty());
-        fldEmail.textProperty().bindBidirectional(model.getTrenutniKorisnik().emailProperty());
-        fldKorisnickoIme.textProperty().bindBidirectional(model.getTrenutniKorisnik().korisnickoImeProperty());
-        fldLozinka.textProperty().bindBidirectional(model.getTrenutniKorisnik().lozinkaProperty());*/
-
         listaKorisnika.setItems(model.getKorisnici());
+        model.trenutniKorisnikProperty().addListener(
+                (obs, oldKorisnik, newKorisnik) -> {
+                    if(oldKorisnik != null) {
+                        fldIme.textProperty().bindBidirectional(oldKorisnik.imeProperty());
+                        fldPrezime.textProperty().bindBidirectional(oldKorisnik.prezimeProperty());
+                        fldEmail.textProperty().bindBidirectional(oldKorisnik.emailProperty());
+                        fldKorisnickoIme.textProperty().bindBidirectional(oldKorisnik.korisnickoImeProperty());
+                        fldLozinka.textProperty().bindBidirectional(oldKorisnik.lozinkaProperty());
+                    }
+                    if(newKorisnik==null) {
+                        fldIme.setText("");
+                        fldPrezime.setText("");
+                        fldEmail.setText("");
+                        fldKorisnickoIme.setText("");
+                        fldLozinka.setText("");
+                    }
+                    else {
+                        fldIme.textProperty().bindBidirectional(newKorisnik.imeProperty());
+                        fldPrezime.textProperty().bindBidirectional(newKorisnik.prezimeProperty());
+                        fldEmail.textProperty().bindBidirectional(newKorisnik.emailProperty());
+                        fldKorisnickoIme.textProperty().bindBidirectional(newKorisnik.korisnickoImeProperty());
+                        fldLozinka.textProperty().bindBidirectional(newKorisnik.lozinkaProperty());
+                    }
+                }
+        );
     }
 
     public void dodajNovogkorisnika(ActionEvent actionEvent) {
+
+        listaKorisnika.getSelectionModel().getSelectedItem();
+        //model.setTrenutniKorisnik(listaKorisnika.getSelectionModel());
     }
 
     public void zatvoriProzor(ActionEvent actionEvent) {
