016edc90b80603614f2b7add7ebaa2648160bc89
Elma <etrako1@etf.unsa.ba>
2019-04-13 22:24:41 +0200

    korisnikModel klasa

---
+++ src/sample/KorisnikModel.java
@@ -0,0 +1,45 @@
+package sample;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class KorisnikModel {
+
+    private ObservableList<Korisnik> korisnici;
+    private SimpleObjectProperty<Korisnik> trenutniKorisnik;
+
+    public KorisnikModel() {
+        this.korisnici=FXCollections.observableArrayList();
+        this.trenutniKorisnik=new SimpleObjectProperty<>();
+    }
+
+    public ObservableList<Korisnik> getKorisnici() {
+        return korisnici;
+    }
+
+    public void setKorisnici(ObservableList<Korisnik> korisnici) {
+        this.korisnici=korisnici;
+    }
+
+    public Korisnik getTrenutniKorisnik() {
+        return trenutniKorisnik.get();
+    }
+
+    public SimpleObjectProperty<Korisnik> trenutniKorisnikProperty() {
+        return trenutniKorisnik;
+    }
+
+    public void setTrenutniKorisnik(Korisnik trenutniKorisnik) {
+        this.trenutniKorisnik.set(trenutniKorisnik);
+    }
+
+    public void napuni() {
+        korisnici.add(new Korisnik("Vedran", "Ljubović", "vljubovic@etf.unsa.na", "vljubovic", "1234"));
+        korisnici.add(new Korisnik("Amra", "Delić", "adelic@etf.unsa.na", "adelic", "5467"));
+        korisnici.add(new Korisnik("Tarik", "Sijerčić", "tsijercic@etf.unsa.na", "tsijercic", "8912"));
+        korisnici.add(new Korisnik("Rijad", "Fejzić", "rfejzic@etf.unsa.na", "rfejzic", "5468"));
+    }
+
+}
